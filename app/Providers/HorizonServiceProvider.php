334cc9dd972afb6d8cc628e42602b8a4ce84294e
Nuno França <contato@nunotech.com.br>
2023-12-28 18:06:22 -0300

    ..

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                //
+                'contato@nunotech.com.br'
             ]);
         });
     }
